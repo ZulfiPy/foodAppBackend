9650f769462c1fe8d413e91e10bf798df940d6dc
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-02-27 05:40:36 +0200

    cors and json use

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import express, { Request, Response } from "express";
 import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/api/myUserRoute";
 
 const app = express();
 
-app.use('/test', async (req: Request, res: Response) => {
-    res.send('hello world')
-})
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.DATABASE_URI;
 
-app.listen(process.env.PORT, () => {
-    console.log(`server runs on port ${process.env.PORT}`)
+mongoose.connect(MONGODB_URI as string).then(() => {
+    console.log('mongo database is connected');
+});
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/api/my/user', myUserRoute);
+
+app.listen(PORT, () => {
+    console.log(`server runs on port ${PORT}`)
 });
